bdc563b7b8a3651d786951212b1a1e8f8c45ee75
Scott Andrew Bedis <bedisscottandrew@gmail.com>
2024-05-30 13:26:53 +0800

    feat: added supabase ssr

---
+++ src/routes/(app)/course/+page.server.ts
@@ -0,0 +1,8 @@
+// import type { PageServerLoad } from './$types';
+
+// export const load: PageServerLoad = async ({ locals: { supabase } }) => {
+// 	const data = await supabase.from('courses').select('*').limit(5).order('title');
+
+//     console.log(data);
+// 	return {};
+// };
